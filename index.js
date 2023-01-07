a8db3cf0594c27b5bb5c3de05811ead826718277
unknown <geoffreyyyagustin@gmail.com>
2023-01-07 00:39:06 -0800

    deleted POST and PUT commands that used Mongoose

--- index.js
+++ index.js
@@ -42,51 +42,6 @@ app.get("/posts", function (req, res) {
    });
 });
 
-// adds a new post to the database
-app.post("/posts", function (req, res) {
-   // Destrucutring i.e. extracting the data from req.body
-   const {
-      id,
-      one,
-      two,
-      four,
-      s_hurdles,
-      l_hurdles,
-      o_relay,
-      f_relay,
-      eight,
-      sixteen,
-      thirty_two,
-   } = req.body;
-   // Creating a new Post object
-   const post = new Post({
-      id: id,
-      one: one,
-      two: two,
-      four: four,
-      s_hurdles: s_hurdles,
-      l_hurdles: l_hurdles,
-      o_relay: o_relay,
-      f_relay: f_relay,
-      eight: eight,
-      sixteen: sixteen,
-      thirty_two: thirty_two,
-   });
-   /*  Saving the new post object into the database. This
-        returns the newly saved mongodb document.
-    */
-   post.save(function (err, newPost) {
-      if (err) {
-         /*  If any error occurs while saving the document,
-                return the error message
-            */
-         return res.status(500).json({ error: err.message });
-      }
-      // Else return message 'Post saved'
-      res.status(200).json({ msg: "Post saved" });
-   });
-});
-
 // get a post through its id
 app.get("/posts/:postID", function (req, res) {
    // Extracting postID from URL
@@ -103,47 +58,6 @@ app.get("/posts/:postID", function (req, res) {
       if (post == null) return res.status(200).json({ msg: "No post found" });
       res.status(200).json(post);
    });
-});
-
-// updates post details (marks needed for a point value)
-app.put("/posts/:postID", function (req, res) {
-   // Extracting postID from URL
-   const postID = req.params.postID;
-   const {
-      one,
-      two,
-      four,
-      s_hurdles,
-      l_hurdles,
-      o_relay,
-      f_relay,
-      eight,
-      sixteen,
-      thirty_two,
-   } = req.body;
-   Post.findOneAndUpdate(
-      { id: postID },
-      { one: one },
-      { two: two },
-      { four: four },
-      { s_hurdles: s_hurdles },
-      { l_hurdles: l_hurdles },
-      { o_relay: o_relay },
-      { f_relay: f_relay },
-      { eight: eight },
-      { sixteen: sixteen },
-      { thirty_two: thirty_two },
-      function (err, post) {
-         if (err) {
-            /* If any error occurs getting the document, then
-               return the error message
-            */
-            return res.status(500).json({ error: err.message });
-         }
-         // Else return msg "Marks updated"
-         res.status(200).json({ msg: "Marks updated" });
-      }
-   );
 });
 
 // deletes selected post from database
