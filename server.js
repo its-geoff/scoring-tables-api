eb26feb838ab10964b0b7303d3de40766910125c
its-geoff <geoffrey.agustin04@gmail.com>
2026-03-22 20:23:12 -0700

    resolved dependency issues and updated connection string for MongoDB

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // initializing dependencies
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
+const marksRouter = require("./routes/marks");
 
 // telling the server to accept any incoming data in JSON format only
 app.use(express.json());
@@ -16,14 +18,14 @@ mongoose
       useNewUrlParser: true,
       useUnifiedTopology: true,
    })
-   .then(() => console.log("Connected to MongoDB"))
+   .then(() => {
+      console.log("Connected to MongoDB");
+      app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+   })
    .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
    res.send("Welcome to Scoring Tables API.");
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-const marksRouter = require("./routes/marks");
 app.use("/marks", marksRouter);
